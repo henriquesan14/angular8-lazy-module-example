3382f9092193dcecd708390a2778a332fe6ee1ff
Henrique <ricogx@hotmail.com>
2019-06-01 23:38:43 -0300

    mudança no layout e listagem prontas

--- src/app/pedido/pedido.module.ts
+++ src/app/pedido/pedido.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PedidoComponent } from './list-pedidos/pedido.component';
 import { PedidoRoutingModule } from './pedido.routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [PedidoComponent],
   imports: [
     CommonModule,
-    PedidoRoutingModule
+    PedidoRoutingModule,
+    SharedModule
   ]
 })
 export class PedidoModule { }
--- src/app/produto/produto.module.ts
+++ src/app/produto/produto.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProdutoComponent } from './list-produtos/produto.component';
 import { ProdutoRoutingModule } from './produto.routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ProdutoComponent],
   imports: [
     CommonModule,
-    ProdutoRoutingModule
+    ProdutoRoutingModule,
+    SharedModule
   ]
 })
 export class ProdutoModule { }
